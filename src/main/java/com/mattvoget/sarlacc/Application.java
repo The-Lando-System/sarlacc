617f66fa556b26b6b60855c53a72e79c15e78f81
Matt Voget <matt.voget@gmail.com>
2016-10-15 10:50:01 -0600

    added request mapping to get the principal user

--- src/main/java/com/mattvoget/sarlacc/Application.java
+++ src/main/java/com/mattvoget/sarlacc/Application.java
@@ -4,13 +4,22 @@ import com.mattvoget.sarlacc.config.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@RestController
 @Import({WebSecurityConfig.class})
 public class Application {
 
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-	
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
 }
